43a2cdfbee8ca341ffb152e7729a13193c199369
mathura <mathura>
2009-08-07 22:37:37 +0000

    added transform(InputStream) method
    SVN-Revision: 51

--- cadsrbulkloader/src/main/java/gov/nih/nci/ncicb/cadsr/bulkloader/transformer/unmarshall/TransformerUnMarshaller.java
+++ cadsrbulkloader/src/main/java/gov/nih/nci/ncicb/cadsr/bulkloader/transformer/unmarshall/TransformerUnMarshaller.java
@@ -1,8 +1,10 @@
 package gov.nih.nci.ncicb.cadsr.bulkloader.transformer.unmarshall;
 
 import java.io.File;
+import java.io.InputStream;
 
 public interface TransformerUnMarshaller {
 
 	public TransformerUnMarshallResult read(File inputFile);
+	public TransformerUnMarshallResult read(InputStream is);
 }
