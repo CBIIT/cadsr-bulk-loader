26ddf5757e3529f5e8bddef454e2c2b8e886aa5d
mathura <mathura>
2009-08-07 22:40:51 +0000

    tests cleanup
    SVN-Revision: 54

--- cadsrbulkloader/src/test/java/gov/nih/nci/ncicb/cadsr/bulkloader/transformer/ExcelTransformerTestCase.java
+++ cadsrbulkloader/src/test/java/gov/nih/nci/ncicb/cadsr/bulkloader/transformer/ExcelTransformerTestCase.java
@@ -1,13 +1,11 @@
 package gov.nih.nci.ncicb.cadsr.bulkloader.transformer;
 
-import gov.nih.nci.ncicb.cadsr.MainTestCase;
 import gov.nih.nci.ncicb.cadsr.bulkloader.excel.transformer.beans.ExcelForm;
 import gov.nih.nci.ncicb.cadsr.bulkloader.excel.transformer.beans.ExcelQuestion;
 import gov.nih.nci.ncicb.cadsr.bulkloader.excel.transformer.unmarshall.ExcelUnMarshaller;
 import gov.nih.nci.ncicb.cadsr.bulkloader.transformer.unmarshall.TransformerUnMarshallResult;
 import gov.nih.nci.ncicb.cadsr.bulkloader.transformer.unmarshall.TransformerUnMarshaller;
 
-import java.io.File;
 import java.util.List;
 
 import junit.framework.TestCase;
@@ -15,13 +13,13 @@ import junit.framework.TestCase;
 
 public class ExcelTransformerTestCase extends TestCase {
 
-	private static final String dataURL1 = "/gov/nih/nci/ncicb/cadsr/8_29_1.xml.old";
-	private static final String dataURL2 = "/gov/nih/nci/ncicb/cadsr/8_29_3.xml.old";
+	private static final String dataURL1 = "/gov/nih/nci/ncicb/cadsr/8_29_1.xml";
+	private static final String dataURL2 = "/gov/nih/nci/ncicb/cadsr/8_29_3.xml";
 	
 	public void testMultipleFilesTransformation() {
 		TransformerUnMarshaller unmarshaller = new ExcelUnMarshaller();
-		TransformerUnMarshallResult result1 = unmarshaller.read(new File(getClasspath()+dataURL1));
-		TransformerUnMarshallResult result2 = unmarshaller.read(new File(getClasspath()+dataURL2));
+		TransformerUnMarshallResult result1 = unmarshaller.read(ExcelTransformerTestCase.class.getResourceAsStream(dataURL1));
+		TransformerUnMarshallResult result2 = unmarshaller.read(ExcelTransformerTestCase.class.getResourceAsStream(dataURL2));
 		
 		assertNotNull(result1);
 		assertNotNull(result2);
@@ -38,10 +36,4 @@ public class ExcelTransformerTestCase extends TestCase {
 		assertEquals(questions1.size(), questions2.size());
 	}
 	
-	protected String getClasspath() {
-		ClassLoader classLoader = ExcelTransformerTestCase.class.getClassLoader();
-		String filePath = classLoader.getResource(".").getPath();
-		
-		return filePath;
-	}
 }
