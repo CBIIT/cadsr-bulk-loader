5c086b5a401fe4091b4e50a34302917613b44aaa
mathura2 <mathura2>
2010-03-03 23:14:07 +0000

    added sourceExists method
    SVN-Revision: 162

--- src/main/java/gov/nih/nci/ncicb/cadsr/bulkloader/dao/read/BulkLoaderReadDAO.java
+++ src/main/java/gov/nih/nci/ncicb/cadsr/bulkloader/dao/read/BulkLoaderReadDAO.java
@@ -45,5 +45,6 @@ public interface BulkLoaderReadDAO {
 	public Context findContextByName(String contextName);
 	
 	public List<String> getAlternateNameTypes();
+	public boolean sourceExists(String sourceName);
 
 }
